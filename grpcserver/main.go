19949a9fbeeaf9948d6bfefd374c648e64923b25
RyotaroSeto <hahahahawaiwai321@gmail.com>
2024-03-17 00:12:21 +0900

    Add gRPC health check and graceful server shutdown

--- grpcserver/main.go
+++ grpcserver/main.go
@@ -1,17 +1,21 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net"
+	"os"
+	"os/signal"
+	"syscall"
 
 	pb "otel-go-sample/proto"
 
+	"google.golang.org/grpc/health"
+	healthpb "google.golang.org/grpc/health/grpc_health_v1"
+	"google.golang.org/grpc/reflection"
+
 	"go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc"
 	"google.golang.org/grpc"
-)
-
-const (
-	port = ":8080"
 )
 
 type helloServer struct {
@@ -19,16 +23,32 @@ type helloServer struct {
 }
 
 func main() {
-	lis, err := net.Listen("tcp", port)
+	ln, err := net.Listen("tcp", ":8080")
 	if err != nil {
 		log.Fatalf("Failed to start server %v", err)
 	}
-	grpcServer := grpc.NewServer(
+	log.Printf("Server started at %v", ln.Addr())
+
+	srv := grpc.NewServer(
 		grpc.StatsHandler(otelgrpc.NewServerHandler()),
 	)
-	pb.RegisterGreetServiceServer(grpcServer, &helloServer{})
-	log.Printf("Server started at %v", lis.Addr())
-	if err := grpcServer.Serve(lis); err != nil {
-		log.Fatalf("Failed to start: %v", err)
-	}
+	svc := &helloServer{}
+	pb.RegisterGreetServiceServer(srv, svc)
+	healthSrv := health.NewServer()
+	healthpb.RegisterHealthServer(srv, healthSrv)
+	reflection.Register(srv)
+
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM, syscall.SIGKILL)
+	defer cancel()
+
+	go func() {
+		if err := srv.Serve(ln); err != nil {
+			log.Fatalf("Failed to serve gRPC server, err: %v", err)
+		}
+	}()
+
+	<-ctx.Done()
+
+	srv.GracefulStop()
+	log.Println("ProductCatalogService gRPC server stopped")
 }
